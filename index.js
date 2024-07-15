f948e6fa612fdc3f486c04ae18e482c5c0ce9b9d
Silvanos Eric <ericoderito@gmail.com>
2024-07-15 22:04:50 +0300

    fix(index.js): ensure fetchBooks returns fetch promise
    Modify fetchBooks function to return its fetch promise, ensuring that it
    can be used in asynchronous operations and that it complies with the
    requirements for testability.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  fetch("https://anapioficeandfire.com/api/books")
+  return fetch("https://anapioficeandfire.com/api/books")
     .then((response) => response.json())
     .then((data) => renderBooks(data));
 }
